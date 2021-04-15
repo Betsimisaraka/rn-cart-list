ff3fe6c063990cef890a16e776fb0a95b5f033bd
Anita RASOA <anita.ras@onja.org>
2021-04-15 11:13:55 +0300

    Changed the icon when it is favorited

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -3,16 +3,18 @@ import { View, Text, SafeAreaView, ScrollView } from 'react-native';
 
 import nachos from '../data/nachos';
 
-import ListItem, { Separator } from '../component/app/ListItem';
+import ListItem, { Separator } from '../components/ListItem';
 
 export default () => {
     return (
         <SafeAreaView>
             <ScrollView>
-                {nachos.map((item)=>(
+                {nachos.map((item, index)=>(
                     <React.Fragment key={item.id}>
                         <ListItem 
                             name={item.name}
+                            onFavoritePress={() => alert('todo: handle favorite')}
+                            isFavorite={index < 2}
                         />
                         <Separator />
                     </React.Fragment>
